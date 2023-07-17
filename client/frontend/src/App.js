0e6c86e146c332f53172f37d84350db380a7cb5e
Caden <cadenkulp@gmail.com>
2023-07-17 10:54:40 -0700

    endpoint for movie title query param

--- client/frontend/src/App.js
+++ client/frontend/src/App.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import MovieContainer from './components/MovieContainer';
+import SearchBar from './components/SearchBar';
 
 import './App.css';
 
@@ -8,10 +10,11 @@ const App = () => {
   
 
   return (
-    <div className="App">
+    <Container className='mt-5'>
+      <SearchBar />
       <h1>Movies</h1>
       <MovieContainer />
-    </div>
+    </Container>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -8,11 +8,31 @@ const port = 3001;
 app.use(cors());
 
 app.get('/movies', (req, res) => {
-  knex('movie')
-    .select('*')
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(404).json(err))
+  const { movieTitle } = req.query;
+  console.log(movieTitle)
+  if (movieTitle !== undefined) {
+    knex('movie')
+      .select('*')
+      .where('title', movieTitle)
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(404).json(err))
+  } else {
+    knex('movie')
+      .select('*')
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(404).json(err))
+  }
 });
+
+// /movies?movie=<movieTitle>
+// app.get('/movies/:movieTitle', (req, res) => {
+//   const { movieTitle } = req.params
+//   knex('movie')
+//     .select('*')
+//     .where('title', movieTitle)
+//     .then(data => res.status(200).json(data))
+//     .catch(err => res.status(404).json(err))
+// })
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
